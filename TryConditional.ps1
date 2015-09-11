bd2464d6549783c00172b20bc6118d9516df3d91
dfinke <finked@hotmail.com>
2015-09-11 11:40:19 -0400

    Initial commit

---
+++ TryConditional.ps1
@@ -0,0 +1,18 @@
+$xlFile = "c:\temp\testCF.xlsx"
+rm $xlFile
+
+$data = Get-Process | where Company | select Company,pm,handles,name
+
+$cfHandles = New-ConditionalFormattingIconSet `
+    -Address "C:C" `
+    -ConditionalFormat ThreeIconSet `
+    -IconType Flags -Reverse
+
+$cfPM = New-ConditionalFormattingIconSet `
+    -Address "B:B" `
+    -ConditionalFormat FourIconSet `
+    -Reverse -IconType TrafficLights
+
+
+$data |
+    Export-Excel -AutoSize -AutoFilter -ConditionalFormat $cfHandles, $cfPM -Path $xlFile -Show
