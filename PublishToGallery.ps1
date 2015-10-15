2b973be4683ec2933e8f86febf2897c702d0d9fa
dfinke <finked@hotmail.com>
2015-10-15 12:48:02 -0400

    Updated for publishing to gallery

--- PublishToGallery.ps1
+++ PublishToGallery.ps1
@@ -4,7 +4,7 @@ $p = @{
     NuGetApiKey = $NuGetApiKey
     LicenseUri = "https://github.com/dfinke/ImportExcel/blob/master/LICENSE.txt"
     Tag = "Excel","EPPlus","Export","Import"
-    ReleaseNote = "ValidateSet for Encoding and Extension"
+    ReleaseNote = "For Import-Excel: added -NoHeader parameter and returns .Value"
     ProjectUri = "https://github.com/dfinke/ImportExcel"
 }
 
