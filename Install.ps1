b4bc13d48ec865b558464a88afc2d06325506bff
dfinke <finked@hotmail.com>
2015-12-17 17:12:19 -0500

    Updated to include additional ps1 files

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,6 @@
 param([string]$InstallDirectory)
 
-$fileList = echo EPPlus.dll ImportExcel.psd1 ImportExcel.psm1 Export-Excel.ps1 New-ConditionalFormattingIconSet.ps1 Export-ExcelSheet.ps1
+$fileList = echo EPPlus.dll ImportExcel.psd1 ImportExcel.psm1 Export-Excel.ps1 New-ConditionalFormattingIconSet.ps1 Export-ExcelSheet.ps1 New-ExcelChart.ps1
 
 if ('' -eq $InstallDirectory)
 {
