4b0efa55f309635ba0cc6956aa99c9b60d97e303
Doug Finke <finked@hotmail.com>
2016-01-05 09:38:55 -0500

    Update HyperLink.ps1

--- Examples/SpreadsheetCells/HyperLink.ps1
+++ Examples/SpreadsheetCells/HyperLink.ps1
@@ -2,7 +2,7 @@ rm *.xlsx
 
 $(
     New-PSItem '=Hyperlink("http://dougfinke.com/blog","Doug Finke")' @("Link")
-    New-PSItem '=Hyperlink("http://blogs.msdn.com/b/powershell/","PowerShell Blog")' @("Link")
-    New-PSItem '=Hyperlink("http://blogs.technet.com/b/heyscriptingguy/","Hey, Scripting Guy")' @("Link")
+    New-PSItem '=Hyperlink("http://blogs.msdn.com/b/powershell/","PowerShell Blog")'
+    New-PSItem '=Hyperlink("http://blogs.technet.com/b/heyscriptingguy/","Hey, Scripting Guy")'
     
 ) | Export-Excel hyperlink.xlsx -AutoSize -Show 
