9879f7a137e1af45a7d384b7e2a6774b271fb4b3
dfinke <finked@hotmail.com>
2016-02-17 19:58:55 -0500

    Prep for publish

--- ImportExcel.psd1
+++ ImportExcel.psd1
@@ -4,7 +4,7 @@
 RootModule = 'ImportExcel.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.97'
+ModuleVersion = '1.98'
 
 # ID used to uniquely identify this module
 GUID = '60dd4136-feff-401a-ba27-a84458c57ede'
--- PublishToGallery.ps1
+++ PublishToGallery.ps1
@@ -2,10 +2,7 @@
 $p = @{
     Name = "ImportExcel"
     NuGetApiKey = $NuGetApiKey
-    #LicenseUri = "https://github.com/dfinke/ImportExcel/blob/master/LICENSE.txt"
-    #Tag = "Excel","EPPlus","Export","Import"
-    ReleaseNote = "Added Conditional Text Formatting"
-    #ProjectUri = "https://github.com/dfinke/ImportExcel"
+    ReleaseNote = "Fixes and great features"
 }
 
 Publish-Module @p
