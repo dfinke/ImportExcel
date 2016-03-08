84fc3e997086dd95cb355358827d65443be45552
dfinke <finked@hotmail.com>
2016-03-08 09:10:11 -0500

    Show Fibonacci series by generating formulas from PS

---
+++ Examples/Fibonacci/ShowFibonacci.ps1
@@ -0,0 +1,19 @@
+param ($fibonacciDigits=10)
+
+
+$file = "fib.xlsx"
+rm "fib.xlsx" -ErrorAction Ignore
+
+
+
+$(
+    New-PSItem 0
+    New-PSItem 1
+    
+    (
+        2..$fibonacciDigits |
+            ForEach {
+                New-PSItem ('=a{0}+a{1}' -f ($_+1),$_)
+            }
+    )
+) | Export-Excel $file -Show
