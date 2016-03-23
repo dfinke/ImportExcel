a436daea7de2c5fa3a155564686fe564ebfb2fda
dfinke <finked@hotmail.com>
2016-03-22 23:41:39 -0400

    Added

--- Examples/Extra/Get-ModuleStats.ps1
+++ Examples/Extra/Get-ModuleStats.ps1
@@ -15,6 +15,6 @@ param(
 $galleryUrl = "https://www.powershellgallery.com/packages/$moduleName"
 $nolegend = '-nolegend'
 if($chartType -eq 'pie') {$nolegend = $null}
-$code = "$($chartType)Chart (Get-HtmlTable $galleryUrl 0) -title 'Download stats for $moduleName' $nolegend"
+$code = "$($chartType)Chart (Get-HtmlTable $galleryUrl 0 | sort lastupdated -desc) -title 'Download stats for $moduleName' $nolegend"
 
 $code | Invoke-Expression
