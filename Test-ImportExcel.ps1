9d14993d8aad3c55a1541ac309157a5220b3066a
W1M0R <W1M0R@users.noreply.github.com>
2016-04-18 00:06:00 +0200

    Added Test-PsVersion and Test-ImportExcel.

---
+++ Test-ImportExcel.ps1
@@ -0,0 +1,40 @@
+<#
+    .SYNOPSIS
+
+    Runs the Pester tests for the ImportExcel module.
+    
+    .DESCRIPTION
+    
+    This function depends on the Pester module, that can be installed via
+    Install-Module. It may be necessary to do an Import-Module Pester
+    before running this function. Alternatively, add Pester to the
+    RequiredModules section of your own script module (.psd1).
+
+    If changes are made to the ImportExcel module, it may be necessary to
+    run Import-Module ImportExcel -Force, before running this function, to
+    ensure that the latest changes are tested.
+
+    .EXAMPLE
+
+    PS> Test-ImportExcel
+    Runs every Pester test in the module (i.e. all files named *.Tests.ps1).
+
+    .EXAMPLE
+
+    PS> Test-ImportExcel -TestName "Export-Excel"
+    Runs the Pester test in the module that describes itself as "Export-Excel".
+
+#>
+function Test-ImportExcel {
+    [CmdletBinding()]
+    param([string]$TestName)
+    Push-Location $PSScriptRoot
+    try {
+        Invoke-Pester -TestName $TestName
+    }
+    catch {
+    }
+    finally {
+        Pop-Location
+    }
+}
---
+++ Test-PsVersion.ps1
@@ -0,0 +1,31 @@
+<#
+    .SYNOPSIS
+
+    Tests for a particular PowerShell version.
+
+    .PARAMETER Version
+
+    The version to test for.
+
+    .EXAMPLE
+
+    PS> Test-PsVersion 4
+
+    This will return true if the PowerShell version is 4 or greater.
+#>
+function Test-PsVersion {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [double]$Version
+    )
+    $psver = $PSVersionTable.PSVersion
+    $check = $null
+    if ($psver.Major -ne $null) {
+        $check = $psver.Major
+    }
+    else {
+        $check = $psver
+    }
+    ([double]"$check") -ge $Version
+}
