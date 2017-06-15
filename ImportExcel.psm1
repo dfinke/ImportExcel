26f6df71680dfdce51c0f905dd46c9561419a793
dfinke <finked@hotmail.com>
2017-06-14 22:44:11 -0400

    Added  Export-ExcelAsSQLInsert

--- ImportExcel.psm1
+++ ImportExcel.psm1
@@ -20,6 +20,7 @@ Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
 . $PSScriptRoot\TrackingUtils.ps1
 . $PSScriptRoot\Copy-ExcelWorkSheet.ps1
 . $PSScriptRoot\Set-CellStyle.ps1
+. $PSScriptRoot\Export-ExcelAsSQLInsert.ps1
 
 if($PSVersionTable.PSVersion.Major -ge 5) {
     . $PSScriptRoot\plot.ps1
