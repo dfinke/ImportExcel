ceb986b408fb7ef230f81ff3083d4cc6db2e204d
dfinke <finked@hotmail.com>
2017-10-13 09:17:51 -0400

    added

---
+++ Examples/TryMultiplePivotTablesFromOneSheet.ps1
@@ -0,0 +1,19 @@
+﻿$file="C:\Temp\test.xlsx"
+
+rm $file -ErrorAction Ignore -Force
+
+
+$ptd = @{}
+$base=@{
+    SourceWorkSheet='gsv'
+    PivotData= @{'Status'='count'}
+    IncludePivotChart=$true
+    ChartType='BarClustered3D'
+}
+
+$ptd.gpt1 = $base + @{ PivotRows = "ServiceType" }
+$ptd.gpt2 = $base + @{ PivotRows = "Status" }
+$ptd.gpt3 = $base + @{ PivotRows = "StartType" }
+$ptd.gpt4 = $base + @{ PivotRows = "CanStop" }
+
+gsv | Export-Excel -path $file -WorkSheetname gsv -Show -PivotTableDefinition $ptd
