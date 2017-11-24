21d9c56854a7bcaeb84b853076fa6f61c7d6b869
dfinke <finked@hotmail.com>
2017-11-24 14:13:30 -0500

    updated to image example

--- Examples/XlRangeToImage/XlRangeToImage.ps1
+++ Examples/XlRangeToImage/XlRangeToImage.ps1
@@ -6,14 +6,18 @@ $xlFileName = "C:\Temp\testPNG.xlsx"
 
 rm C:\Temp\testPNG.xlsx -ErrorAction Ignore
 
-@"
+$range = @"
 Region,Item,Cost
 North,Pear,1
 South,Apple,2
 East,Grapes,3
 West,Berry,4
+North,Pear,1
+South,Apple,2
+East,Grapes,3
+West,Berry,4
 "@ | ConvertFrom-Csv | 
-    Export-Excel $xlFileName `
+    Export-Excel $xlFileName -ReturnRange `
         -ConditionalText (New-ConditionalText Apple), (New-ConditionalText Berry -ConditionalTextColor White -BackgroundColor Purple) 
 
-Convert-XlRangeToImage -Path $xlFileName -workSheetname sheet1 -range A1:C5 -Show
+Convert-XlRangeToImage -Path $xlFileName -workSheetname sheet1 -range $range -Show
