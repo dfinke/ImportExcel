f3808e49b96b648436be90578941ed996d0a5eb1
tehfonz <tehfonz1@gmail.com>
2017-12-21 16:00:05 +1100

    Update Get-HTMLTable
    Added logic to check whether the file is local (UNC Path or Local Drive) or Web Hosted (via regex). Minor changes to the way the for loop works to account for the new objects and to make sure they are both compatible with either local or web hosted html tables.

--- Get-HtmlTable.ps1
+++ Get-HtmlTable.ps1
@@ -1,42 +1,52 @@
 function Get-HtmlTable {
-    param(
-        [Parameter(Mandatory=$true)]
-        $url,
-        $tableIndex=0,
-        $Header,
-        [int]$FirstDataRow=0,
-        [Switch]$UseDefaultCredentials
-    )
+	param(
+		[Parameter(Mandatory = $true)]
+		$url,
+		$tableIndex = 0,
+		$Header,
+		[int]$FirstDataRow = 0,
+		[Switch]$UseDefaultCredentials
+	)
+	if ($url -match '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www\.)') {
+		$r = Invoke-WebRequest $url -UseDefaultCredentials: $UseDefaultCredentials
 
-    $r = Invoke-WebRequest $url -UseDefaultCredentials: $UseDefaultCredentials
-    
-    $table = $r.ParsedHtml.getElementsByTagName("table")[$tableIndex]
-    $propertyNames=$Header
-    $totalRows=@($table.rows).count
+		$table = $r.ParsedHtml.getElementsByTagName("table")[$tableIndex]
+	}
+	else {
+		$r = New-Object -ComObject "HTMLFile"
+		$source = Get-Content -Path $url -Raw
+		$r.IHTMLDocument2_write($source)
+		
+		$table = $r.getElementsByTagName("table").Item($tableIndex)
+	}
+	
+	$propertyNames = $Header
 
-    for ($idx = $FirstDataRow; $idx -lt $totalRows; $idx++) {
+	for ($idx = $FirstDataRow; $idx -lt @($table.rows).count; $idx++) {
 
-        $row = $table.rows[$idx]
-        $cells = @($row.cells)        
+		$row = $table.rows.item($idx)
+		$cells = @($row.cells)
 
-        if(!$propertyNames) {
-            if($cells[0].tagName -eq 'th') {
-                $propertyNames = @($cells | foreach {$_.innertext -replace ' ',''})
-            } else  {
-                $propertyNames =  @(1..($cells.Count + 2) | % { "P$_" })
-            }
-            continue
-        }        
+		if (!$propertyNames) {
+			if ($cells[0].tagName -eq 'TD') {
+				$propertyNames = @($cells | ForEach-Object {$_.innertext -replace ' ', ''})
+			}
+			else {
+				$propertyNames = @(1..($cells.Count + 2) | ForEach-Object { "P$_" })
+			}
+			continue
+		}
 
-        $result = [ordered]@{}
+		$result = [ordered]@{}
 
-        for($counter = 0; $counter -lt $cells.Count; $counter++) {
-            $propertyName = $propertyNames[$counter]
+		for ($counter = 0; $counter -lt $cells.Count; $counter++) {
+			$propertyName = $propertyNames[$counter]
 
-            if(!$propertyName) { $propertyName= '[missing]'}
-            $result.$propertyName= $cells[$counter].InnerText
-        }
+			if (!$propertyName) { $propertyName = '[missing]'}
+			$result.$propertyName = $cells[$counter].InnerText
+		}
 
-        [PSCustomObject]$result
-    }
+		[PSCustomObject]$result
+	}
 }
+
