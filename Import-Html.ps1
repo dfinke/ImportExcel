eceeeb913acf9cf5372adba5ceef43b6d099f9d7
tehfonz <tehfonz1@gmail.com>
2018-02-02 12:54:15 +1100

    Some more changes
    Added a regex param validator for the $Path parameter: It must have 'xlsx' in the path or the function will not work, the Export-Excel function requires the file be in xlsx format. 
    
    Added some logic around if the path is set, and Append is not so that the user is aware they may be overwriting an already existing file and if they would like to proceed. Yes will overwrite the file, No will break.
    
    $TempFile variable is added to account for the Append state, if a temp file is used append will have no affect anyway, so is set to $False. If $TempFile is set to $True and Append is used it will set the $AppendState = $True.
    
    If $Show is not set and $TempFile is set to $False the path is sent to the clipboard and written to the console (via Write-Host).
    
    Export-Excel is at the end of the Get-HTMLTable Pipeline instead of setting a variable

--- Import-Html.ps1
+++ Import-Html.ps1
@@ -4,6 +4,7 @@ function Import-Html {
 		$url,
 		$index,
 		$Header,
+		[ValidatePattern('xlsx')]
 		[System.IO.FileInfo]$Path,
 		[Switch]$Append,
 		[int]$FirstDataRow = 0,
@@ -12,13 +13,29 @@ function Import-Html {
 	)
 	
 	if ($Path) {
+		$TempFile = $False
 		if ((Test-Path $Path) -and -not ($Append)) {
-			Remove-Item $Path -Confirm
+			
+			$Message = 'File already exists, would you like to overwrite this file'
+			$Question = 'Selecting Yes will overwrite file, No will exit'
+
+			$Choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+			$Choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+			$Choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+		
+			$Decision = $Host.UI.PromptForChoice($Message, $Question, $Choices, 1)
+			if ($Decision -eq 0) {
+				Remove-Item $Path -Force -Confirm:$False
+			}
+			else {
+				break
+			}
 		}
 	}
 	else {
 		$Path = [System.IO.Path]::GetTempFileName() -replace "tmp", "xlsx"
-		Remove-Item $Path -ErrorAction Ignore	
+		Remove-Item $Path -ErrorAction Ignore
+		$TempFile = $True
 	}
 	
 	if ($Show) {
@@ -26,12 +43,21 @@ function Import-Html {
 	}
 	else {
 		$OpenExcel = $False
+		Write-Host "Exporting to Excel file $($Path)"
+		if ($TempFile -eq $True) {
+		Write-Verbose "Setting clipboard to $($Path)"
+		$Path.FullName | clip
+		}
+	}
+	
+	if ($Append -and $TempFile -eq $False) {
+		$AppendState = $True
+	}
+	else {
+		$AppendState = $False
 	}
 	
 	Write-Verbose "Exporting to Excel file $($Path)"
 
-	$data = Get-HtmlTable -url $url -tableIndex $index -Header $Header -FirstDataRow $FirstDataRow -UseDefaultCredentials: $UseDefaultCredentials
-
-	if ($Append -and $Path -notmatch 'temp') {$data | Export-Excel -Path $Path -AutoSize -Append -Show:$OpenExcel}
-	else {$data | Export-Excel -Path $Path -AutoSize -Show:$OpenExcel}
+	Get-HtmlTable -url $url -tableIndex $index -Header $Header -FirstDataRow $FirstDataRow -UseDefaultCredentials: $UseDefaultCredentials | Export-Excel -Path $Path -AutoSize -Append:$AppendState -Show:$OpenExcel
 }
