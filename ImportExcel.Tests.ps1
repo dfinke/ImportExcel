dfinke/ImportExcel

commit bd40cfe829d6128ac39296805ed5c41ff424bc0f

Append, and formatting
Changed Export-Excel.ps1

#1 @ Line 197 Made new parameter sets . Default, and table already
existed and use path. Added DefaultPackage and TablePackage
A New parameter  "Package" allows an ExcelPackage object returned by
-passThru to be passed in
~Line 400 code to use package or path depending on path passed.

(also added Open-ExcelPackage  to get the object without exporting and
Close-ExcelPackage to close it nicely - these are in their own file)

#2.  @ Line 256 added new parameter excludeProperty to remove unwanted
properties without needing to go through select-object
~Line 459 added logic to exclude the properties specified in the new
parameter

#3 .  @ Line 262 Added new parameter Append
~Line 420 code to read the existing headers and move the insertion point
below the current data
(normal behaviour is to check if headers exist when adding data in the
process block, which makes this change wonderfully easy)
~Line 510 changed basis for identifying named ranges and changed scope
for rangeName so it can be used on other sheets

#4.  ~Line 550. Remove any existing Pivot table before trying to
[re]create it.

Added formatting.ps1 which applies conditional and normal formats -
requires an ExcelPackage to be open.

Added Open-ExcelPackage.ps1 (which contains a close function as well to
get the the object and save it )  open allows the sheet to be loaded
into a package object without needing to export .

Updated .psm1 to add the formating and open/close ps1 files.

ImportExcel.psm1
@@ -1,4 +1,4 @@
-Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
+ï»¿Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
 
 . $PSScriptRoot\Charting.ps1
 . $PSScriptRoot\ConvertFromExcelData.ps1
@@ -7,6 +7,7 @@ Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
 . $PSScriptRoot\Copy-ExcelWorkSheet.ps1
 . $PSScriptRoot\Export-Excel.ps1
 . $PSScriptRoot\Export-ExcelSheet.ps1
+. $PSScriptRoot\Formatting.ps1
 . $PSScriptRoot\Get-ExcelColumnName.ps1
 . $PSScriptRoot\Get-ExcelSheetInfo.ps1
 . $PSScriptRoot\Get-ExcelWorkbookInfo.ps1
@@ -20,16 +21,20 @@ Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
 . $PSScriptRoot\New-ConditionalText.ps1
 . $PSScriptRoot\New-ExcelChart.ps1
 . $PSScriptRoot\New-PSItem.ps1
+. $PSScriptRoot\Open-ExcelPackage.ps1      
 . $PSScriptRoot\Pivot.ps1
 . $PSScriptRoot\Set-CellStyle.ps1
 . $PSScriptRoot\TrackingUtils.ps1
 . $PSScriptRoot\Update-FirstObjectProperties.ps1
+   
 
 
 if ($PSVersionTable.PSVersion.Major -ge 5) {
     . $PSScriptRoot\Plot.ps1
 
     Function New-Plot {
+        [OutputType([PSPlot])]
+        Param()
 
         [PSPlot]::new()
     }
@@ -46,11 +51,11 @@ Function Import-Excel {
         Create custom objects from the rows in an Excel worksheet.
  
     .DESCRIPTION 
-        The Import-Excel cmdlet creates custom objects from the rows in an Excel worksheet. Each row represents one object. All of this is possible without installing Microsoft Excel and by using the .NET library ‘EPPLus.dll’.
+        The Import-Excel cmdlet creates custom objects from the rows in an Excel worksheet. Each row represents one object. All of this is possible without installing Microsoft Excel and by using the .NET library â€˜EPPLus.dllâ€™.
 
         By default, the property names of the objects are retrieved from the column headers. Because an object cannot have a blanc property name, only columns with column headers will be imported.
 
-        If the default behavior is not desired and you want to import the complete worksheet ‘as is’, the parameter ‘-NoHeader’ can be used. In case you want to provide your own property names, you can use the parameter ‘-HeaderName’. 
+        If the default behavior is not desired and you want to import the complete worksheet â€˜as isâ€™, the parameter â€˜-NoHeaderâ€™ can be used. In case you want to provide your own property names, you can use the parameter â€˜-HeaderNameâ€™. 
 
     .PARAMETER Path 
         Specifies the path to the Excel file.
@@ -71,18 +76,18 @@ Function Import-Excel {
     .PARAMETER NoHeader
         Automatically generate property names (P1, P2, P3, ..) instead of the ones defined in the column headers of the TopRow.
 
-        This switch is best used when you want to import the complete worksheet ‘as is’ and are not concerned with the property names.
+        This switch is best used when you want to import the complete worksheet â€˜as isâ€™ and are not concerned with the property names.
 
     .PARAMETER StartRow
         The row from where we start to import data, all rows above the StartRow are disregarded. By default this is the first row. 
 
-        When the parameters ‘-NoHeader’ and ‘-HeaderName’ are not provided, this row will contain the column headers that will be used as property names. When one of both parameters are provided, the property names are automatically created and this row will be treated as a regular row containing data.
+        When the parameters â€˜-NoHeaderâ€™ and â€˜-HeaderNameâ€™ are not provided, this row will contain the column headers that will be used as property names. When one of both parameters are provided, the property names are automatically created and this row will be treated as a regular row containing data.
 
     .PARAMETER Password
         Accepts a string that will be used to open a password protected Excel file.
 
     .EXAMPLE
-        Import data from an Excel worksheet. One object is created for each row. The property names of the objects consist of the column names defined in the first row. In case a column doesn’t have a column header (usually in row 1 when ‘-StartRow’ is not used), then the unnamed columns will be skipped and the data in those columns will not be imported.
+        Import data from an Excel worksheet. One object is created for each row. The property names of the objects consist of the column names defined in the first row. In case a column doesnâ€™t have a column header (usually in row 1 when â€˜-StartRowâ€™ is not used), then the unnamed columns will be skipped and the data in those columns will not be imported.
 
         ----------------------------------------------
         | File: Movies.xlsx     -      Sheet: Actors |
@@ -104,7 +109,7 @@ Function Import-Excel {
         Notice that column 'B' is not imported because there's no value in cell 'B1' that can be used as property name for the objects.
 
     .EXAMPLE
-        Import the complete Excel worksheet ‘as is’ by using the ‘-NoHeader’ switch. One object is created for each row. The property names of the objects will be automatically generated (P1, P2, P3, ..).
+        Import the complete Excel worksheet â€˜as isâ€™ by using the â€˜-NoHeaderâ€™ switch. One object is created for each row. The property names of the objects will be automatically generated (P1, P2, P3, ..).
 
         ----------------------------------------------
         | File: Movies.xlsx     -      Sheet: Actors |
@@ -132,7 +137,7 @@ Function Import-Excel {
         Notice that the column header (row 1) is imported as an object too.
  
      .EXAMPLE
-        Import data from an Excel worksheet. One object is created for each row. The property names of the objects consist of the names defined in the parameter ‘-HeaderName’. The properties are named starting from the most left column (A) to the right. In case no value is present in one of the columns, that property will have an empty value.
+        Import data from an Excel worksheet. One object is created for each row. The property names of the objects consist of the names defined in the parameter â€˜-HeaderNameâ€™. The properties are named starting from the most left column (A) to the right. In case no value is present in one of the columns, that property will have an empty value.
 
         ----------------------------------------------------------
         | File: Movies.xlsx            -           Sheet: Movies |
@@ -169,7 +174,7 @@ Function Import-Excel {
         Notice that empty rows are imported and that data for the property 'Genre' is not present in the worksheet. As such, the 'Genre' property will be blanc for all objects.
         
      .EXAMPLE
-        Import data from an Excel worksheet. One object is created for each row. The property names of the objects are automatically generated by using the switch ‘-NoHeader’ (P1, P@, P#, ..). The switch ‘-DataOnly’ will speed up the import because empty rows and empty columns are not imported.
+        Import data from an Excel worksheet. One object is created for each row. The property names of the objects are automatically generated by using the switch â€˜-NoHeaderâ€™ (P1, P@, P#, ..). The switch â€˜-DataOnlyâ€™ will speed up the import because empty rows and empty columns are not imported.
 
         ----------------------------------------------------------
         | File: Movies.xlsx            -           Sheet: Movies |
@@ -181,7 +186,7 @@ Function Import-Excel {
         |4     Skyfall         2012           9                  |
         ----------------------------------------------------------
 
-        PS C:\> Import-Excel -Path 'C:\Movies.xlsx' -WorkSheetname Movies –NoHeader -DataOnly
+        PS C:\> Import-Excel -Path 'C:\Movies.xlsx' -WorkSheetname Movies â€“NoHeader -DataOnly
  
         P1: The Bodyguard
         P2: 1992
@@ -198,7 +203,7 @@ Function Import-Excel {
         Notice that empty rows and empty columns are not imported.
 
  .EXAMPLE
-        Import data from an Excel worksheet. One object is created for each row. The property names are provided with the ‘-HeaderName’ parameter. The import will start from row 2 and empty columns and rows are not imported.
+        Import data from an Excel worksheet. One object is created for each row. The property names are provided with the â€˜-HeaderNameâ€™ parameter. The import will start from row 2 and empty columns and rows are not imported.
 
         ----------------------------------------------------------
         | File: Movies.xlsx            -           Sheet: Actors |
@@ -209,13 +214,13 @@ Function Import-Excel {
         |3     Jean-Claude               Vandamme     Brussels   |
         ----------------------------------------------------------
 
-        PS C:\> Import-Excel -Path 'C:\Movies.xlsx' -WorkSheetname Actors -DataOnly -HeaderName 'FirstName', 'SecondName', 'City' –StartRow 2
+        PS C:\> Import-Excel -Path 'C:\Movies.xlsx' -WorkSheetname Actors -DataOnly -HeaderName 'FirstName', 'SecondName', 'City' â€“StartRow 2
          
         FirstName : Jean-Claude
         SecondName: Vandamme
         City      : Brussels
 
-        Notice that only 1 object is imported with only 3 properties. Column B and row 2 are empty and have been disregarded by using the switch '-DataOnly'. The property names have been named with the values provided with the parameter '-HeaderName'. Row number 1 with ‘Chuck Norris’ has not been imported, because we started the import from row 2 with the parameter ‘-StartRow 2’.
+        Notice that only 1 object is imported with only 3 properties. Column B and row 2 are empty and have been disregarded by using the switch '-DataOnly'. The property names have been named with the values provided with the parameter '-HeaderName'. Row number 1 with â€˜Chuck Norrisâ€™ has not been imported, because we started the import from row 2 with the parameter â€˜-StartRow 2â€™.
             
     .LINK
         https://github.com/dfinke/ImportExcel

Export-Excel.ps1
@@ -1,4 +1,4 @@
-Function Export-Excel {
+﻿Function Export-Excel {
     <#
         .SYNOPSIS
             Export data to an Excel worksheet.
@@ -196,8 +196,13 @@ Function Export-Excel {
 
     [CmdletBinding(DefaultParameterSetName = 'Default')]
     Param(
-        $Path,
-        [Parameter(ValueFromPipeline = $true)]
+        [Parameter(Mandatory=$true,ParameterSetName="Default",Position=0)] 
+        [Parameter(Mandatory=$true,ParameterSetName="Table"  ,Position=0)] 
+        [String]$Path, 
+        [Parameter(Mandatory=$true,ParameterSetName="PackageDefault")] 
+        [Parameter(Mandatory=$true,ParameterSetName="PackageTable")] 
+        [OfficeOpenXml.ExcelPackage]$ExcelPackage,     
+        [Parameter(ValueFromPipeline=$true)]
         $TargetData,
         [String]$WorkSheetname = 'Sheet1',
         [String]$Title,
@@ -225,6 +230,7 @@ Function Export-Excel {
         [Switch]$FreezeTopRowFirstColumn,
         [Int[]]$FreezePane,
         [Parameter(ParameterSetName = 'Default')]
+        [Parameter(ParameterSetName = 'PackageDefault')]
         [Switch]$AutoFilter,
         [Switch]$BoldTopRow,
         [Switch]$NoHeader,
@@ -244,8 +250,10 @@ Function Export-Excel {
                 }
             })]
         [Parameter(ParameterSetName = 'Table')]
+        [Parameter(ParameterSetName = 'PackageTable')]
         [String]$TableName,
         [Parameter(ParameterSetName = 'Table')]
+        [Parameter(ParameterSetName = 'PackageTable')]
         [OfficeOpenXml.Table.TableStyles]$TableStyle = 'Medium6',
         [Object[]]$ExcelChartDefinition,
         [String[]]$HideSheet,
@@ -255,11 +263,13 @@ Function Export-Excel {
         [Int]$StartColumn = 1,
         [Switch]$PassThru,
         [String]$Numberformat = 'General',
+        [string[]]$ExcludeProperty,
         [String[]]$NoNumberConversion,
         [Object[]]$ConditionalFormat,
         [Object[]]$ConditionalText,
         [ScriptBlock]$CellStyleSB,
-        [Switch]$Now
+        [Switch]$Now,
+        [switch]$Append
     )
 
     Begin {
@@ -354,7 +364,7 @@ Function Export-Excel {
             if ($TitleBackgroundColor -AND ($TitleFillPattern -ne 'None')) {
                 $ws.Cells[$Row, $StartColumn].Style.Fill.BackgroundColor.SetColor($TitleBackgroundColor)
             }
-            else {
+            elseif ($TitleBackgroundColor)  {
                 Write-Warning "Title Background Color ignored. You must set the TitleFillPattern parameter to a value other than 'None'. Try 'Solid'."
             }
         }
@@ -403,35 +413,52 @@ Function Export-Excel {
                 }
             }
 
-            $Path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
+            if ($ExcelPackage) { 
+                $pkg           = $ExcelPackage 
+                $Path          = $pkg.File 
+            } 
+            Else               {
+                $Path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
 
-            if (Test-Path $Path) {
-                Write-Debug "Path '$Path' already exists"
-            }
+                if (Test-Path $Path) {
+                    Write-Debug "Path '$Path' already exists"
+                }
 
-            $pkg = New-Object OfficeOpenXml.ExcelPackage $Path
-            $ws = $pkg | Add-WorkSheet -WorkSheetname $WorkSheetname -NoClobber:$NoClobber
-
+                $pkg = New-Object OfficeOpenXml.ExcelPackage $Path
+            }  
+            
+            [OfficeOpenXml.ExcelWorksheet]$ws  = $pkg | Add-WorkSheet -WorkSheetname $WorkSheetname -NoClobber:$NoClobber
+            
             foreach ($format in $ConditionalFormat ) {
                 $target = "Add$($format.Formatter)"
                 $rule = ($ws.ConditionalFormatting).PSObject.Methods[$target].Invoke($format.Range, $format.IconType)
                 $rule.Reverse = $format.Reverse
             }
 
-            $Row = $StartRow
-
-            if ($Title) {
-                Add-Title
-
-                $Row += 1
+            if ($append)       {
+                $headerRange       = $ws.Dimension.Address -replace "\d+$","1"
+                #if there is a title or anything else above the header row, specifying StartRow will skip it. 
+                if ($StartRow -ne 1) {$headerRange  = $headerRange -replace "1","$StartRow"} 
+                $script:Header     = $ws.Cells[$headerrange].Value
+                $row               = $ws.Dimension.Rows  
+                Write-Debug -Message ("Appending: headers are " + ($script:Header -join ", ") + "Start row $row") 
             }
-
+            elseif($Title)     {    #Can only add a title if not appending              
+                $Row = $StartRow
+                Add-Title
+                $Row ++
+            }
+            else {
+                $Row = $StartRow
+            
+            } 
+            $ColumnIndex = $StartColumn
             $firstTimeThru = $true
             $isDataTypeValueType = $false
             $pattern = 'string|bool|byte|char|decimal|double|float|int|long|sbyte|short|uint|ulong|ushort'
         }
         Catch {
-            if ($AlreadyExists) {
+            if ($AlreadyExists) {  #Is this set anywhere ? 
                 throw "Failed exporting worksheet '$WorkSheetname' to '$Path': The worksheet '$WorkSheetname' already exists."
             }
             else {
@@ -460,7 +487,7 @@ Function Export-Excel {
                 #region Add headers
                 if (-not $script:Header) {
                     $ColumnIndex = $StartColumn
-                    $script:Header = $TargetData.PSObject.Properties.Name
+                   $script:Header = $TargetData.PSObject.Properties.Name | Where-Object {$_ -notin $ExcludeProperty}  
 
                     if ($NoHeader) {
                         # Don't push the headers to the spread sheet
@@ -536,7 +563,9 @@ Function Export-Excel {
                 $cer = $ws.Dimension.End.Row
                 $cec = $script:Header.Count
 
-                $targetRange = $ws.Cells[$csr, $csc, $cer, $cec]
+                $targetRange = $ws.Cells[$csr, $csc, $cer, $cec] 
+                #if we're appending data the table may already exist: but excel doesn't like the result if I put 
+                # if ($ws.Tables[$TableName]) {$ws.Tables.Delete($TableName) } 
                 $tbl = $ws.Tables.Add($targetRange, $TableName)
                 $tbl.TableStyle = $TableStyle
             }
@@ -545,6 +574,8 @@ Function Export-Excel {
                 foreach ($item in $PivotTableDefinition.GetEnumerator()) {
                     $targetName = $item.Key
                     $pivotTableName = $targetName #+ 'PivotTable'
+                    #Make sure the Pivot table sheet doesn't already exist
+                    try  {     $pkg.Workbook.Worksheets.Delete(    $pivotTableName) } catch {} 
                     $wsPivot = $pkg | Add-WorkSheet -WorkSheetname $pivotTableName -NoClobber:$NoClobber
                     $pivotTableDataName = $targetName + 'PivotTableData'
 
@@ -611,6 +642,8 @@ Function Export-Excel {
 
             if ($IncludePivotTable) {
                 $pivotTableName = $WorkSheetname + 'PivotTable'
+                #Make sure the Pivot table sheet doesn't already exist
+                try  {     $pkg.Workbook.Worksheets.Delete(    $pivotTableName) } catch {} 
                 $wsPivot = $pkg | Add-WorkSheet -WorkSheetname $pivotTableName -NoClobber:$NoClobber
 
                 $wsPivot.View.TabSelected = $true

Open-ExcelPackage.ps1
@@ -1,0 +1,50 @@
+﻿Function Open-ExcelPackage  {
+<#
+.Synopsis 
+    Returns an Excel Package Object with for the specified XLSX ile 
+.Example
+    $excel  = Open-ExcelPackage -path $xlPath 
+    $sheet1 = $excel.Workbook.Worksheets["sheet1"] 
+    set-Format -Address $sheet1.Cells["E1:S1048576"], $sheet1.Cells["V1:V1048576"]  -NFormat ([cultureinfo]::CurrentCulture.DateTimeFormat.ShortDatePattern) 
+    close-ExcelPackage $excel -Show
+
+   This will open the file at $xlPath, select sheet1 apply formatting to two blocks of the sheet and close the package 
+#>
+    [OutputType([OfficeOpenXml.ExcelPackage])]
+    Param ([Parameter(Mandatory=$true)]$path,
+           [switch]$KillExcel)
+
+        if($KillExcel)         {
+            Get-Process -Name "excel" -ErrorAction Ignore | Stop-Process
+            while (Get-Process -Name "excel" -ErrorAction Ignore) {}
+        }
+
+        $Path          = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
+        if (Test-Path $path) {New-Object -TypeName OfficeOpenXml.ExcelPackage -ArgumentList $Path }
+        Else                 {Write-Warning "Could not find $path" } 
+ }
+
+Function Close-ExcelPackage {
+<#
+.Synopsis 
+    Closes an Excel Package, saving, saving under a new name or abandoning changes and opening the file as required 
+#>
+    Param (
+    #File to close
+    [parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [OfficeOpenXml.ExcelPackage]$ExcelPackage,
+    #Open the file
+    [switch]$Show 
+    #Abandon the file without saving
+    [Switch]$NoSave,
+    #Save file with a new name (ignored if -NoSaveSpecified)
+    $SaveAs, 
+    )
+    if ( $NoSave)      {$ExcelPackage.Dispose()}
+    else {
+          if ($SaveAs) {$ExcelPackage.SaveAs( $SaveAs ) } 
+          Else         {$ExcelPackage.Save(); $SaveAs = $ExcelPackage.File.FullName }
+          $ExcelPackage.Dispose() 
+          if ($show)   {Start-Process -FilePath $SaveAs } 
+    }
+}

commit 33d86cb3c96b242ae65f14236601dd8bc2c83b8d

Fixed the bug "Unable to find type [PSPlot]"
Also added a test for it in ImportExcel.Tests.ps1

The previous fix was broke again by commit:
https://github.com/dfinke/ImportExcel/commit/bd40cfe829d6128ac39296805ed5c41ff424bc0f#diff-f50d26003c0baf98f417a44fc1cac92b

ImportExcel.Tests.ps1
@@ -2071,3 +2071,14 @@ Context 'special cases' {
         }
     }
 }
+
+Context 'General Tests' {
+    in $TestDrive {
+        Describe 'Get Help' {
+            it 'New-Plot' {
+                #Get-Help : Unable to find type [PSPlot].
+                {Help New-Plot} | Should -Not -Throw
+            }
+        }
+    }
+}

ImportExcel.psm1
@@ -39,7 +39,6 @@ if ($PSVersionTable.PSVersion.Major -ge 5) {
     . $PSScriptRoot\Plot.ps1
 
     Function New-Plot {
-        [OutputType([PSPlot])]
         Param()
 
         [PSPlot]::new()