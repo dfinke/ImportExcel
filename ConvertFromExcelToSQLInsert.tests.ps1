2e8c69ea6b9bba7fffee81378c9a55a0ca5a2272
dfinke <finked@hotmail.com>
2018-04-22 14:12:44 -0400

    added tests

---
+++ ConvertFromExcelToSQLInsert.tests.ps1
@@ -0,0 +1,36 @@
+﻿cls
+
+ipmo .\ImportExcel.psd1 -Force
+
+$xlFile = ".\testSQL.xlsx"
+
+Describe "ConvertFrom-ExcelToSQLInsert" {
+    
+    BeforeEach {
+        
+        $([PSCustomObject]@{
+            Name="John"
+            Age=$null
+        }) | Export-Excel $xlFile
+    }
+
+    AfterAll {
+        rm $xlFile -Recurse -Force -ErrorAction Ignore
+    }
+
+    It "Should be empty double single quotes" {            
+        $expected="INSERT INTO Sheet1 ('Name', 'Age') Values('John', '');"
+ 
+        $actual = ConvertFrom-ExcelToSQLInsert -Path $xlFile Sheet1 
+        
+        $actual | should be $expected
+    }
+
+     It "Should have NULL" {    
+        $expected="INSERT INTO Sheet1 ('Name', 'Age') Values('John', NULL);"
+ 
+        $actual = ConvertFrom-ExcelToSQLInsert -Path $xlFile Sheet1 -ConvertEmptyStringsToNull
+        
+        $actual | should be $expected
+    }
+}
