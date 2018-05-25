893b2f321476bea547a67850abae2fcddaf80954
dfinke <finked@hotmail.com>
2018-05-25 16:32:33 -0400

    tightened up the example

--- Examples/JustCharts/CentralLimitTheorem.ps1
+++ Examples/JustCharts/CentralLimitTheorem.ps1
@@ -1,9 +1,9 @@
-﻿ColumnChart -Title "Central Limit Theorem" ($(
-    for ($i = 1; $i -le 500; $i++) { 
-        $s=0
-        for ($j = 1; $j -le 100; $j++){ 
-            $s+=Get-Random -Minimum 0 -Maximum 2
+﻿ColumnChart -Title "Central Limit Theorem" -NoLegend ($(
+        for ($i = 1; $i -le 500; $i++) {
+            $s = 0
+            for ($j = 1; $j -le 100; $j++) {
+                $s += Get-Random -Minimum 0 -Maximum 2
+            }
+            $s
         }
-        $s
-    }
-) | Sort | Group | select Count, Name)
+    ) | Sort-Object | Group-Object | Select-Object Count, Name)
