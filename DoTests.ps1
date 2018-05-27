e09f2b55635d01735c8ff88e2eac1c2c42ccc847
dfinke <finked@hotmail.com>
2018-05-27 12:54:44 -0400

    Force install of pester

--- DoTests.ps1
+++ DoTests.ps1
@@ -1,10 +1,9 @@
 
-Get-Module -ListAvailable pester | Out-Host
+# Get-Module -ListAvailable pester | Out-Host
+# return
+# if ((Get-Module -ListAvailable pester) -eq $null) {
+#     Install-Module -Name Pester -Repository PSGallery -Force
+# }
 
-
-return
-if ((Get-Module -ListAvailable pester) -eq $null) {
-    Install-Module -Name Pester -Repository PSGallery -Force
-}
-
-Invoke-Pester -Script $PSScriptRoot\UnitTests\
+Install-Module -Name Pester -Repository PSGallery -Force
+Invoke-Pester -Script $PSScriptRoot\UnitTests
