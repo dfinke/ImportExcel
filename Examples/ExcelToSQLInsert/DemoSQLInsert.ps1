b2a884e7f1ab78acd2bd18a2b33e57fc1489ae89
dfinke <finked@hotmail.com>
2018-06-24 09:57:00 -0400

    Demo of the `-UseMSSQLSyntax` parameter

---
+++ Examples/ExcelToSQLInsert/DemoSQLInsert.ps1
@@ -0,0 +1,7 @@
+Import-Module ..\..\ImportExcel.psd1 -Force
+
+
+ConvertFrom-ExcelToSQLInsert -TableName "Movies" -Path ".\Movies.xlsx" -ConvertEmptyStringsToNull
+''
+'# UseMSSQLSyntax'
+ConvertFrom-ExcelToSQLInsert -UseMSSQLSyntax -TableName "Movies" -Path ".\Movies.xlsx" -ConvertEmptyStringsToNull
