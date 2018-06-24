fa887bbb248f00b15adda4a8b8e21cf6d21db011
dfinke <finked@hotmail.com>
2018-06-24 08:38:34 -0400

    Added example demos -AutoNameRange and PowerShell injecting Excel formulas

---
+++ Examples/SpreadsheetCells/ExcelFormulasUsingAddMember.ps1
@@ -0,0 +1,12 @@
+Remove-Item .\testFormula.xlsx -ErrorAction Ignore
+
+@"
+id,item,units,cost
+12001,Nails,37,3.99
+12002,Hammer,5,12.10
+12003,Saw,12,15.37
+12010,Drill,20,8
+12011,Crowbar,7,23.48
+"@ | ConvertFrom-Csv |
+    Add-Member -PassThru -MemberType NoteProperty -Name Total -Value "=units*cost" |
+    Export-Excel -Path .\testFormula.xlsx -Show -AutoSize -AutoNameRange
