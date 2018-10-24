6b2190bfff5c66aed1a06090c04345db464bd1fa
Conrad Agramont <41589731+conradagramont@users.noreply.github.com>
2018-10-24 16:37:10 -0700

    Updated AddMultiWorkSheet
    Removed the extra code that wasn't required.

--- Examples/AddWorkSheet/AddMultiWorkSheet.ps1
+++ Examples/AddWorkSheet/AddMultiWorkSheet.ps1
@@ -1,25 +1,15 @@
 try {. $PSScriptRoot\..\..\LoadPSD1.ps1} catch {}
 
 $xlSourcefile = "$env:TEMP\Source.xlsx"
+write-host "Save location: $xlSourcefile"
 
 Remove-Item $xlSourcefile -ErrorAction Ignore
 
 #Put some simple data in a worksheet and Get an excel package object to represent the file
-$excel = 1..10 | Export-Excel $xlSourcefile -PassThru
-
-#Add a new worksheet named 'NewSheet' and copying the sheet that was just made (Sheet1) to the new sheet
-#Add-WorkSheet -ExcelPackage $excel -WorkSheetname "NewSheet" -CopySource $excel.Workbook.Worksheets["Sheet1"]
-#Save and open in Excel
-#Close-ExcelPackage -ExcelPackage $excel -Show
-
-#Put some simple data in a worksheet and Get an excel package object to represent the file
-$TabData1 = 1..10 | Export-Excel $xlSourcefile -PassThru -WorksheetName 'Tab 1' -AutoSize -AutoFilter
-Close-ExcelPackage -ExcelPackage $TabData1 
+$TabData1 = 1..5 | Export-Excel $xlSourcefile -WorksheetName 'Tab 1' -AutoSize -AutoFilter
 
 #Add another tab.  Replace the $TabData2 with your data
-$TabData2 = 1..10 | Export-Excel $xlSourcefile -PassThru -WorksheetName 'Tab 2' -AutoSize -AutoFilter
-Close-ExcelPackage -ExcelPackage $TabData2
+$TabData2 = 1..10 | Export-Excel $xlSourcefile -WorksheetName 'Tab 2' -AutoSize -AutoFilter
 
 #Add another tab.  Replace the $TabData3 with your data
-$TabData3 = 1..10  | Export-Excel $xlSourcefile -PassThru -WorksheetName 'Tab 3' -AutoSize -AutoFilter
-Close-ExcelPackage -ExcelPackage $TabData3 -Show
+$TabData3 = 1..15  | Export-Excel $xlSourcefile -WorksheetName 'Tab 3' -AutoSize -AutoFilter -Show
