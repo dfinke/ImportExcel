77481f2901649b6d2b2882d504909f0c1b159560
jhoneill <jhoneill@waitrose.com>
2018-11-30 16:18:46 +0000

    More of the same

--- DoTests.ps1
+++ DoTests.ps1
@@ -16,8 +16,8 @@ $ModuleVersion = (Get-Content -Raw .\ImportExcel.psd1)  | Invoke-Expression | Fo
 
 if (!$DontCreateZip) {
     if ($null -eq $env:BUILD_ARTIFACTSTAGINGDIRECTORY) {$env:BUILD_ARTIFACTSTAGINGDIRECTORY = '.'}
-    $dest = "ImportExcel-{0}-{1}.zip" -f $ModuleVersion, (Get-Date).ToString("yyyyMMddHHmmss")
-    Compress-Archive -Path $env:BUILD_ARTIFACTSTAGINGDIRECTORY -DestinationPath .\$dest  -Verbose
+    $dest = Join-Path -Path $env:BUILD_ARTIFACTSTAGINGDIRECTORY -ChildPath ("ImportExcel-{0}-{1}.zip" -f $ModuleVersion, (Get-Date).ToString("yyyyMMddHHmmss"))
+    Compress-Archive -Path . -DestinationPath .\$dest  -Verbose
 }
 
 if ($null -eq (Get-Module -ListAvailable pester)) {
