787dda70ee6460ab348f282a83087417cd73721d
jhoneill <jhoneill@waitrose.com>
2018-12-06 20:16:34 +0000

    Revert "Take verbose back out of DoTests"
    This reverts commit 268d48ce3d947d2da7a5456b52bb1acd5b224572.

--- DoTests.ps1
+++ DoTests.ps1
@@ -9,6 +9,7 @@ param(
 $PSVersionTable.PSVersion
 $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 
+$VerbosePreference = "Continue"
 ## Create the zip before the tests run
 ## Otherwise the EPPlus.dll is in use after the Pester run
 $ModuleVersion = (Get-Content -Raw .\ImportExcel.psd1)  | Invoke-Expression | ForEach-Object ModuleVersion
