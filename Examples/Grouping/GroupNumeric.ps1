8c5b3b2f5f1f17ace0418b531d66568ec3e1ae8b
dfinke <finked@hotmail.com>
2018-12-07 18:08:50 -0500

    Added Places and Points examples. Taken from unit tests

---
+++ Examples/Grouping/GroupNumeric.ps1
@@ -0,0 +1,10 @@
+﻿$xlfile = "$env:TEMP\Places.xlsx"
+
+Remove-Item $xlfile -ErrorAction SilentlyContinue
+
+$PivotTableDefinition = New-PivotTableDefinition -Activate -PivotTableName Places `
+    -PivotRows Driver, FinishPosition -PivotData @{Date = "Count"} -GroupNumericRow FinishPosition -GroupNumericMin 1 -GroupNumericMax 25 -GroupNumericInterval 3
+
+Import-Csv "$PSScriptRoot\First10Races.csv" |
+    Select-Object  Race, @{n = "Date"; e = {[datetime]::ParseExact($_.date, "dd/MM/yyyy", (Get-Culture))}}, FinishPosition, Driver, GridPosition, Team, Points |
+    Export-Excel $xlfile -Show -AutoSize -PivotTableDefinition $PivotTableDefinition
