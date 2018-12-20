9c79ba573c51dc4ad71b5dcab9f18f93858b737e
dfinke <finked@hotmail.com>
2018-12-20 09:13:45 -0500

    updated

--- Examples/PivotTable/MultiplePivotTables.ps1
+++ Examples/PivotTable/MultiplePivotTables.ps1
@@ -9,48 +9,48 @@ North,1/1/2018,Pears,100
 East,4/1/2018,Grapes,200
 West,7/1/2018,Bananas,300
 South,10/1/2018,Apples,400
-"@ | Select-Object -Property Region, @{n="Date";e={[datetime]::ParseExact($_.Date,"M/d/yyyy",(Get-Culture))}}, Fruit, Sold
+"@ | Select-Object -Property Region, @{n = "Date"; e = {[datetime]::ParseExact($_.Date, "M/d/yyyy", (Get-Culture))}}, Fruit, Sold
 
-$xlfile="$env:temp\multiplePivotTables.xlsx"
-rm $xlfile -ErrorAction SilentlyContinue
+$xlfile = "$env:temp\multiplePivotTables.xlsx"
+Remove-Item $xlfile -ErrorAction SilentlyContinue
 
 $excel = $data | Export-Excel $xlfile -PassThru -AutoSize -TableName FruitData
 
 $pivotTableParams = @{
-    PivotTableName = "ByRegion" 
-    Address = $excel.Sheet1.cells["F1"] 
+    PivotTableName  = "ByRegion"
+    Address         = $excel.Sheet1.cells["F1"]
     SourceWorkSheet = $excel.Sheet1
-    PivotRows = echo Region Fruit Date
-    PivotData = @{'sold'='sum'} 
+    PivotRows       = echo Region Fruit Date
+    PivotData       = @{'sold' = 'sum'}
     PivotTableStyle = 'Light21'
-    GroupDateRow = "Date"
-    GroupDatePart = echo Years Quarters
+    GroupDateRow    = "Date"
+    GroupDatePart   = echo Years Quarters
 }
 
 $pt = Add-PivotTable @pivotTableParams -PassThru
 #$pt.RowHeaderCaption ="By Region,Fruit,Date"
 $pt.RowHeaderCaption = "By " + ($pivotTableParams.PivotRows -join ",")
 
-$pivotTableParams.PivotTableName="ByFruit"
-$pivotTableParams.Address = $excel.Sheet1.cells["J1"] 
+$pivotTableParams.PivotTableName = "ByFruit"
+$pivotTableParams.Address = $excel.Sheet1.cells["J1"]
 $pivotTableParams.PivotRows = echo Fruit Region Date
 
 $pt = Add-PivotTable @pivotTableParams -PassThru
-$pt.RowHeaderCaption ="By Fruit,Region"
+$pt.RowHeaderCaption = "By Fruit,Region"
 
-$pivotTableParams.PivotTableName="ByDate"
-$pivotTableParams.Address = $excel.Sheet1.cells["N1"] 
-$pivotTableParams.PivotRows = echo Date Region Fruit 
-
-$pt = Add-PivotTable @pivotTableParams -PassThru
-$pt.RowHeaderCaption ="By Date,Region,Fruit"
-
-$pivotTableParams.PivotTableName="ByX"
-$pivotTableParams.Address = $excel.Sheet1.cells["S1"] 
-$pivotTableParams.GroupDatePart = echo Years 
+$pivotTableParams.PivotTableName = "ByDate"
+$pivotTableParams.Address = $excel.Sheet1.cells["N1"]
+$pivotTableParams.PivotRows = echo Date Region Fruit
 
 $pt = Add-PivotTable @pivotTableParams -PassThru
-$pt.RowHeaderCaption ="By Years,Region"
+$pt.RowHeaderCaption = "By Date,Region,Fruit"
+
+$pivotTableParams.PivotTableName = "ByX"
+$pivotTableParams.Address = $excel.Sheet1.cells["S1"]
+$pivotTableParams.GroupDatePart = echo Years
+
+$pt = Add-PivotTable @pivotTableParams -PassThru
+$pt.RowHeaderCaption = "By Years,Region"
 
 
 Set-ExcelColumn -Worksheet $excel.Sheet1 -Column 2 -AutoSize
