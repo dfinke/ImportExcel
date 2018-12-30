56acf56430c200db58a851f2b2e5c9c1330d0912
dfinke <finked@hotmail.com>
2018-12-30 08:52:11 -0500

    Add code to do removal

--- RemoveWorksheet.ps1
+++ RemoveWorksheet.ps1
@@ -1,2 +1,19 @@
 ﻿Function Remove-WorkSheet {
+    param(
+        [Parameter(Mandatory)]
+        $Path,
+        [Parameter(Mandatory)]
+        [String[]]$WorksheetName,
+        [Switch]$Show
+    )
+
+    $pkg = Open-ExcelPackage -Path $Path
+
+    if ($pkg) {
+        foreach ($wsn in $WorksheetName) {
+            $pkg.Workbook.Worksheets.Delete($wsn)
+        }
+
+        Close-ExcelPackage -ExcelPackage $pkg -Show:$Show
+    }
 }
