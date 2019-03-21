7858d886430e5b5be32069d4bc3d7ffec2c03ba6
dfinke <finked@hotmail.com>
2019-03-21 14:35:26 -0400

    Updated link to correct blog #566

--- Examples/SpreadsheetCells/HyperLink.ps1
+++ Examples/SpreadsheetCells/HyperLink.ps1
@@ -3,7 +3,7 @@ try {. $PSScriptRoot\..\..\LoadPSD1.ps1} catch {}
 Remove-Item "$env:temp\hyperlink.xlsx" -ErrorAction SilentlyContinue
 
 $(
-    New-PSItem '=Hyperlink("http://dougfinke.com/blog","Doug Finke")' @("Link")
+    New-PSItem '=Hyperlink("https://dfinke.github.io","Doug Finke")' @("Link")
     New-PSItem '=Hyperlink("http://blogs.msdn.com/b/powershell/","PowerShell Blog")'
     New-PSItem '=Hyperlink("http://blogs.technet.com/b/heyscriptingguy/","Hey, Scripting Guy")'
 
