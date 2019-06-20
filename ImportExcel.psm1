691c977df9d27fe6f45dfa83e8bfe2260d89681d
Mike Hokanson <mhokanson@reboundmd.com>
2019-06-20 09:08:17 -0700

    Fix some issues with the cmdlet re-organization

--- ImportExcel.psm1
+++ ImportExcel.psm1
@@ -1,49 +1,21 @@
 ﻿#region import everything we need
 Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
-<#
-. $PSScriptRoot\AddConditionalFormatting.ps1
-. $PSScriptRoot\AddDataValidation.ps1
-. $PSScriptRoot\Charting.ps1
-. $PSScriptRoot\ColorCompletion.ps1
-. $PSScriptRoot\ConvertExcelToImageFile.ps1
-. $PSScriptRoot\compare-workSheet.ps1
-. $PSScriptRoot\ConvertFromExcelData.ps1
-. $PSScriptRoot\ConvertFromExcelToSQLInsert.ps1
-. $PSScriptRoot\ConvertToExcelXlsx.ps1
-. $PSScriptRoot\Copy-ExcelWorkSheet.ps1
-. $PSScriptRoot\Export-Excel.ps1
-. $PSScriptRoot\Export-ExcelSheet.ps1
-. $PSScriptRoot\Export-StocksToExcel.ps1
-. $PSScriptRoot\Get-ExcelColumnName.ps1
-. $PSScriptRoot\Get-ExcelSheetInfo.ps1
-. $PSScriptRoot\Get-ExcelWorkbookInfo.ps1
-. $PSScriptRoot\Get-HtmlTable.ps1
-. $PSScriptRoot\Get-Range.ps1
-. $PSScriptRoot\Get-XYRange.ps1
-. $PSScriptRoot\Import-Html.ps1
-. $PSScriptRoot\InferData.ps1
-. $PSScriptRoot\Invoke-Sum.ps1
-. $PSScriptRoot\Join-Worksheet.ps1
-. $PSScriptRoot\Merge-worksheet.ps1
-. $PSScriptRoot\New-ConditionalFormattingIconSet.ps1
-. $PSScriptRoot\New-ConditionalText.ps1
-. $PSScriptRoot\New-ExcelChart.ps1
-. $PSScriptRoot\New-PSItem.ps1
-. $PSScriptRoot\Open-ExcelPackage.ps1
-. $PSScriptRoot\Pivot.ps1
-. $PSScriptRoot\PivotTable.ps1
-. $PSScriptRoot\RemoveWorksheet.ps1
-. $PSScriptRoot\Send-SqlDataToExcel.ps1
-. $PSScriptRoot\Set-CellStyle.ps1
-. $PSScriptRoot\Set-Column.ps1
-. $PSScriptRoot\Set-Row.ps1
-. $PSScriptRoot\Set-WorkSheetProtection.ps1
-. $PSScriptRoot\SetFormat.ps1
-. $PSScriptRoot\TrackingUtils.ps1
-. $PSScriptRoot\Update-FirstObjectProperties.ps1
-#>
+
+function Import-ModuleFile
+{
+	[CmdletBinding()]
+	Param (
+		[string]
+		$Path
+	)
+	
+	if ($doDotSource) { . $Path }
+	else { $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($Path))), $null, $null) }
+}
+
+
 # Import all public functions
-foreach ($cmdlet in (Get-ChildItem "$ModuleRoot\cmdlets\*.ps1"))
+foreach ($cmdlet in (Get-ChildItem "$($PSScriptRoot)\cmdlets\*.ps1"))
 {
 	. Import-ModuleFile -Path $cmdlet.FullName
 }
