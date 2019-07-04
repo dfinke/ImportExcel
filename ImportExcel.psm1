914c61048b4beb37ae42fcf60a6d27bb09adfc16
dfinke <finked@hotmail.com>
2019-07-04 12:03:05 -0400

    Moved back, CI has issues with $psscriptroot for xlsx

--- ImportExcel.psm1
+++ ImportExcel.psm1
@@ -301,15 +301,6 @@ function Import-Excel {
     begin {
         $sw = [System.Diagnostics.Stopwatch]::StartNew()
 
-        # $Path = (Resolve-Path $Path).ProviderPath
-        $resolvedPath = (Resolve-Path $Path -ErrorAction SilentlyContinue)
-        if ($resolvedPath) {
-            $Path = $resolvedPath.ProviderPath
-        }
-        else {
-            throw "'$($Path)' file not found"
-        }
-
         Function Get-PropertyNames {
             <#
             .SYNOPSIS
@@ -356,6 +347,15 @@ function Import-Excel {
 
     process {
         if ($path) {
+
+            # $Path = (Resolve-Path $Path).ProviderPath
+            $resolvedPath = (Resolve-Path $Path -ErrorAction SilentlyContinue)
+            if ($resolvedPath) {
+                $Path = $resolvedPath.ProviderPath
+            }
+            else {
+                throw "'$($Path)' file not found"
+            }
 
             $stream = New-Object -TypeName System.IO.FileStream -ArgumentList $Path, 'Open', 'Read', 'ReadWrite'
             if ($Password) { $ExcelPackage = New-Object -TypeName OfficeOpenXml.ExcelPackage -ArgumentList $stream , $Password }
