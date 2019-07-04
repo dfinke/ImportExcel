7faa27a3b3aecd4bd5c1007f29b2149dd130ffbc
dfinke <finked@hotmail.com>
2019-07-04 11:53:59 -0400

    create xlsx before all in cwd

--- __tests__/RelativePath.tests.ps1
+++ __tests__/RelativePath.tests.ps1
@@ -1,22 +1,31 @@
 ﻿Describe "Test reading relative paths" {
+    BeforeAll {
+        $script:xlfileName = "TestR.xlsx"
+        @{data = 1 } | Export-Excel "$pwd\TestR.xlsx"
+    }
+
+    AfterAll {
+        Remove-Item "$pwd\$($script:xlfileName)"
+    }
+
     It "Should read local file" {
-        $actual = Import-Excel -Path ".\testRelative.xlsx"
+        $actual = Import-Excel -Path ".\$($script:xlfileName)"
         $actual | Should Not Be $null
-        $actual.Count | Should Not Be 1
+        $actual.Count | Should Be 1
     }
 
     It "Should read with pwd" {
-        $actual = Import-Excel -Path "$pwd\testRelative.xlsx"
+        $actual = Import-Excel -Path "$pwd\$($script:xlfileName)"
         $actual | Should Not Be $null
     }
 
     It "Should read with PSScriptRoot" {
-        $actual = Import-Excel -Path "$PSScriptRoot\testRelative.xlsx"
+        $actual = Import-Excel -Path "$PSScriptRoot\$($script:xlfileName)"
         $actual | Should Not Be $null
     }
 
     It "Should read with just a file name and resolve to cwd" {
-        $actual = Import-Excel -Path "testRelative.xlsx"
+        $actual = Import-Excel -Path "$($script:xlfileName)"
         $actual | Should Not Be $null
     }
 
