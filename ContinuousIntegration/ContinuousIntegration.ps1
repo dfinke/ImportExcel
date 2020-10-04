63b64742207ba06d6c0be229eaafc509593f873f
dfinke <finked@hotmail.com>
2020-10-04 11:03:13 -0400

    try catch

--- ContinuousIntegration/ContinuousIntegration.ps1
+++ ContinuousIntegration/ContinuousIntegration.ps1
@@ -8,18 +8,23 @@ foreach ($module in $modules) {
     Import-Module $module -Force -PassThru
 }
 
-$pesterResults = Invoke-Pester -Output Detailed -PassThru
+try {
+    $pesterResults = Invoke-Pester -Output Detailed -PassThru
 
-if ($pesterResults.FailedCount -gt 0) {
+    if ($pesterResults.FailedCount -gt 0) {
         
-    '[Progress] Pester Results Failed'
-    $pesterResults.Failed | Out-String
+        '[Progress] Pester Results Failed'
+        $pesterResults.Failed | Out-String
     
-    '[Progress] Pester Results FailedBlocks'
-    $pesterResults.FailedBlocks | Out-String
+        '[Progress] Pester Results FailedBlocks'
+        $pesterResults.FailedBlocks | Out-String
     
-    '[Progress] Pester Results FailedContainers'
-    $pesterResults.FailedContainers | Out-String
+        '[Progress] Pester Results FailedContainers'
+        $pesterResults.FailedContainers | Out-String
 
-    Throw "Tests failed"
+        Throw "Tests failed"
+    }
+}
+catch {
+    Throw "Tests failed"    
 }
