16c44a68bdb1944a1933e255cf25b466eca34afc
Phillips <joel.phillips@untsystem.edu>
2020-10-05 17:57:02 -0500

    corrected organization of pester elements

--- __tests__/Import-Excel-SkipHiddenRows.tests.ps1
+++ __tests__/Import-Excel-SkipHiddenRows.tests.ps1
@@ -1,31 +1,29 @@
-BeforeAll {
-    $WithHiddenRows = "TestDrive:\testImportExcelWithHiddenRows.xlsx"
-    $Header = @('Product','City','Gross','Net')
-    $InputObjectArray = ConvertFrom-Csv -Header $Header -InputObject "
-        Apple,London,300,250
-        Orange,London,400,350
-        Banana,London,300,200
-        Orange,Paris,600,500
-        Banana,Paris,300,200
-        Apple,New York,1200,700
-    "
-    $InputObjectArray | Export-Excel -Path $WithHiddenRows
-    $ExcelPackage = Open-ExcelPackage -Path $WithHiddenRows
-    Set-ExcelRow -ExcelPackage $ExcelPackage -Row 3 -Hide:$true
-    Set-ExcelRow -ExcelPackage $ExcelPackage -Row 4 -Hide:$true
-    Set-ExcelRow -ExcelPackage $ExcelPackage -Row 6 -Hide:$true
-    Close-ExcelPackage -ExcelPackage $ExcelPackage
-}
-Describe "Import-Excel (and show hidden rows by default)" {
+Import-Module $PSScriptRoot\..\ImportExcel.psd1
+Describe "Import-Excel -SkipHiddenRows" {
+    BeforeAll {
+        $WithHiddenRows = "TestDrive:\testImportExcelWithHiddenRows.xlsx"
+        $Header = @('Product','City','Gross','Net')
+        $InputObjectArray = ConvertFrom-Csv -Header $Header -InputObject "
+            Apple,London,300,250
+            Orange,London,400,350
+            Banana,London,300,200
+            Orange,Paris,600,500
+            Banana,Paris,300,200
+            Apple,New York,1200,700
+        "
+        $InputObjectArray | Export-Excel -Path $WithHiddenRows
+        $ExcelPackage = Open-ExcelPackage -Path $WithHiddenRows
+        Set-ExcelRow -ExcelPackage $ExcelPackage -Row 3 -Hide:$true
+        Set-ExcelRow -ExcelPackage $ExcelPackage -Row 4 -Hide:$true
+        Set-ExcelRow -ExcelPackage $ExcelPackage -Row 6 -Hide:$true
+        Close-ExcelPackage -ExcelPackage $ExcelPackage
+    }
     It "Should have all data" {
         $ObjectArray = Import-Excel -Path $WithHiddenRows
         $ObjectArray.Count | Should -Be 6
     }
-}
-
-Describe "Import-Excel -SkipHiddenRows" {
     It "Should have only visible data" {
-        $ObjectArray = Import-Excel -Path $WithHiddenRows
+        $ObjectArray = Import-Excel -Path $WithHiddenRows -SkipHiddenRows
         $ObjectArray.Count | Should -Be 3
     }
 }
