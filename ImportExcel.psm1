4b07f8a0cd8f5e1956a799895eb8e4cd7cb7e413
mhokanson <mhokanson@gmail.com>
2020-11-19 14:02:22 -0800

    Getting caught up

--- ImportExcel.psm1
+++ ImportExcel.psm1
@@ -1,35 +1,8 @@
 ﻿#region import everything we need
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Refresh stale fork
-=======
->>>>>>> f7604088d35804420211092ce66df1629a78fd43
 $culture = $host.CurrentCulture.Name -replace '-\w*$', ''
 Import-LocalizedData  -UICulture $culture -BindingVariable Strings -FileName Strings -ErrorAction Ignore
 if (-not $Strings) {
     Import-LocalizedData  -UICulture "en" -BindingVariable Strings -FileName Strings -ErrorAction Ignore
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-try { [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") }
-catch { Write-Warning -Message $Strings.SystemDrawingAvailable }
-=======
-Add-Type -Path "$($PSScriptRoot)\EPPlus.dll"
-
-function Import-ModuleFile
-{
-	[CmdletBinding()]
-	Param (
-		[string]
-		$Path
-	)
-	
-	if ($doDotSource) { . $Path }
-	else { $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($Path))), $null, $null) }
-=======
->>>>>>> Refresh stale fork
 }
 try { [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") }
 catch { Write-Warning -Message $Strings.SystemDrawingAvailable }
@@ -37,21 +10,7 @@ catch { Write-Warning -Message $Strings.SystemDrawingAvailable }
 foreach ($directory in @('Private', 'Public', 'Charting', 'InferData', 'Pivot')) {
     Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" | ForEach-Object { . $_.FullName }
 }
->>>>>>> Moving cmdlets for organization reasons
 
-<<<<<<< HEAD
-=======
-}
-try { [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") }
-catch { Write-Warning -Message $Strings.SystemDrawingAvailable }
-
->>>>>>> f7604088d35804420211092ce66df1629a78fd43
-foreach ($directory in @('Private', 'Public', 'Charting', 'InferData', 'Pivot')) {
-    Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" | ForEach-Object { . $_.FullName }
-}
-
-=======
->>>>>>> Refresh stale fork
 if ($PSVersionTable.PSVersion.Major -ge 5) {
     . $PSScriptRoot\Plot.ps1
 
