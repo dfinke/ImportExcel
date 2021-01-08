2e2f7a3a9b9b00c17e376e807f81c1592f910197
MikeyBronowski <mikeybronowski@gmail.com>
2021-01-08 17:02:30 +0000

    Add-ExcelHyperlink test

---
+++ __tests__/Add-ExcelHyperlink.tests.ps1
@@ -0,0 +1,34 @@
+Describe "Testing adding hyperlink" {
+    BeforeAll {
+		$path = "TestDrive:\hyperlinkAdd.xlsx"
+        $worksheetName = "Sheet1"
+        $cell = "A2"
+        $hyperlink = "NamedRange"
+
+        Remove-Item -Path $path -ErrorAction SilentlyContinue
+        1..5 | Export-Excel -Path $path -WorksheetName $worksheetName 
+
+        $excelPackage = Open-ExcelPackage -Path $path -KillExcel
+        $excel = $excelPackage.Workbook.Worksheets[$worksheetName]
+
+        $rangeCells=$excel.Cells["D1:E12"]
+        $excelPackage.Workbook.Names.Add($rangeName,$rangeCells)
+
+        Close-ExcelPackage -ExcelPackage $excelPackage
+	}
+	It "Hyperlink does not exist" {
+		$hyperlink = Get-ExcelHyperlink -Path $path -WorksheetName $worksheetName  -Cell $Cell
+		$hyperlink | Should -Be $null
+
+	}
+	It "Hyperlink is been added" {
+		Add-ExcelHyperlink -Path $path -WorksheetName $worksheetName -Cell $Cell -Hyperlink $rangeName -DisplayName 'display text'
+
+		$hyperlink2 = Get-ExcelHyperlink -Path $path -WorksheetName $worksheetName -Cell $Cell 
+        ($hyperlink2).Address| Should -Be $Cell
+	}
+
+	It "Cell style is changed from Normal to Hyperlink" {
+        ($hyperlink2).StyleName| Should -Be 'Hyperlink'
+	}  
+}
