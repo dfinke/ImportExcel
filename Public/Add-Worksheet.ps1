b5ef9afb4ff62e0272b921ff334afdb03fb0d08f
MikeyBronowski <mikeybronowski@gmail.com>
2021-01-09 21:41:44 +0000

    Restore changed to the wrong file

--- Public/Add-Worksheet.ps1
+++ Public/Add-Worksheet.ps1
@@ -1,73 +1,80 @@
-function Add-ExcelHyperlink {
-    <#
-            .SYNOPSIS
-                Add the hyperlink to the specified cell
-    
-            .PARAMETER Path
-                The Excel workbook path
-    
-            .PARAMETER ExcelPackage object
-                The Excel package
-         
-            .PARAMETER WorksheetName
-                The worksheet containing the target cell
-     
-            .PARAMETER Cell
-                Target cell that will have hyperlink set
-         
-            .PARAMETER DisplayName
-                If specified will replace the value of the underlying cell
-    
-            .PARAMETER Show
-                Display the workbook after adding the hyperlink
-    
-            .EXAMPLE
-            
-                Add-ExcelHyperlink -Path C:\Temp\AddHyperlink.xlsx -WorksheetName Sheet1 -Cell A2 -Hyperlink NamedRange -DisplayName 'Text to display'
-            #>
-        [CmdletBinding()]
-        param(
-           
-            [String]$Path,
-            [OfficeOpenXml.ExcelPackage]$ExcelPackage,
-            [String]$WorksheetName,
-            [Parameter(Mandatory)]
-            [String]$Cell,
-            [Parameter(Mandatory)]
-            [String]$Hyperlink,
-            [String]$DisplayName,
-            [switch]$Show
-        )
-        
-            Write-verbose -Message "Opening ExcelPackage via Path [$Path]"
-            if ($Path -and -not $ExcelPackage) { $ExcelPackage = Open-ExcelPackage -Path $Path }
-    
-            Write-verbose -Message "Setting the Worksheet to [$WorksheetName]"
-            $ws = $ExcelPackage.Workbook.Worksheets[$WorksheetName]
-            
-    
-            $cellValue = $ws.Cells[$Cell].Value
-            if (!$DisplayName) {
-                Write-verbose -Message "Keeping the value = [$cellValue] of the [$Cell] cell"
-                $DisplayName = $cellValue
+function Add-Worksheet  {
+    [cmdletBinding()]
+    [OutputType([OfficeOpenXml.ExcelWorksheet])]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "Package", Position = 0)]
+        [OfficeOpenXml.ExcelPackage]$ExcelPackage,
+        [Parameter(Mandatory = $true, ParameterSetName = "Workbook")]
+        [OfficeOpenXml.ExcelWorkbook]$ExcelWorkbook,
+        [string]$WorksheetName ,
+        [switch]$ClearSheet,
+        [Switch]$MoveToStart,
+        [Switch]$MoveToEnd,
+        $MoveBefore ,
+        $MoveAfter ,
+        [switch]$Activate,
+        [OfficeOpenXml.ExcelWorksheet]$CopySource,
+        [parameter(DontShow=$true)]
+        [Switch] $NoClobber
+    )
+    #if we were given a workbook use it, if we were given a package, use its workbook
+    if      ($ExcelPackage -and -not $ExcelWorkbook) {$ExcelWorkbook = $ExcelPackage.Workbook}
+
+    # If WorksheetName was given, try to use that worksheet. If it wasn't, and we are copying an existing sheet, try to use the sheet name
+    # If we are not copying a sheet, and have no name, use the name "SheetX" where X is the number of the new sheet
+    if      (-not $WorksheetName -and $CopySource -and -not $ExcelWorkbook[$CopySource.Name]) {$WorksheetName = $CopySource.Name}
+    elseif  (-not $WorksheetName) {$WorksheetName = "Sheet" + (1 + $ExcelWorkbook.Worksheets.Count)}
+    else    {$ws = $ExcelWorkbook.Worksheets[$WorksheetName]}
+
+    #If -clearsheet was specified and the named sheet exists, delete it
+    if      ($ws -and $ClearSheet) { $ExcelWorkbook.Worksheets.Delete($WorksheetName) ; $ws = $null }
+
+    #Copy or create new sheet as needed
+    if (-not $ws -and $CopySource) {
+          Write-Verbose -Message "Copying into worksheet '$WorksheetName'."
+          $ws = $ExcelWorkbook.Worksheets.Add($WorksheetName, $CopySource)
+    }
+    elseif (-not $ws) {
+          $ws = $ExcelWorkbook.Worksheets.Add($WorksheetName)
+          Write-Verbose -Message "Adding worksheet '$WorksheetName'."
+    }
+    else {Write-Verbose -Message "Worksheet '$WorksheetName' already existed."}
+    #region Move sheet if needed
+    if     ($MoveToStart) {$ExcelWorkbook.Worksheets.MoveToStart($WorksheetName) }
+    elseif ($MoveToEnd  ) {$ExcelWorkbook.Worksheets.MoveToEnd($WorksheetName)   }
+    elseif ($MoveBefore ) {
+        if ($ExcelWorkbook.Worksheets[$MoveBefore]) {
+            if ($MoveBefore -is [int]) {
+                $ExcelWorkbook.Worksheets.MoveBefore($ws.Index, $MoveBefore)
             }
-    
-            Write-verbose -Message "Creating a hyperlink [$Hyperlink] under [$DisplayName]"
-            $hyperlinkObj = New-Object -TypeName OfficeOpenXml.ExcelHyperLink -ArgumentList $Hyperlink , $DisplayName
-            
-            Write-verbose -Message "Adding hyperlink [$Hyperlink] in the [$Cell] cell on [$WorksheetName] worksheet"
-            $null = $ws.Cells[$Cell].Hyperlink = $hyperlinkObj
-    
-            if(($ws.Workbook.Styles.NamedStyles.Name  -EQ 'hyperlink').Count -eq 0) {
-                Write-verbose -Message "The NamedStyle Hyperlink does not exist - creating one"
-                $namedStyle=$ws.Workbook.Styles.CreateNamedStyle("hyperlink")
-                $namedStyle.Style.Font.UnderLine = $true
-                $namedStyle.Style.Font.Color.SetColor("Blue")
-    
-                Write-verbose -Message "Changing [$Cell] cell style from [$($ws.Cells[$Cell].StyleName)] to [$($namedStyle.Name)]"
-                $null = $ws.Cells[$Cell].StyleName = $($namedStyle.Name)
+            else {$ExcelWorkbook.Worksheets.MoveBefore($WorksheetName, $MoveBefore)}
+        }
+        else {Write-Warning "Can't find worksheet '$MoveBefore'; worsheet '$WorksheetName' will not be moved."}
+    }
+    elseif ($MoveAfter  ) {
+        if ($MoveAfter -eq "*") {
+            if ($WorksheetName -lt $ExcelWorkbook.Worksheets[1].Name) {$ExcelWorkbook.Worksheets.MoveToStart($WorksheetName)}
+            else {
+                $i = 1
+                While ($i -lt $ExcelWorkbook.Worksheets.Count -and ($ExcelWorkbook.Worksheets[$i + 1].Name -le $WorksheetName) ) { $i++}
+                $ExcelWorkbook.Worksheets.MoveAfter($ws.Index, $i)
             }
-    
-            Write-verbose -Message "Closing the ExcelPackage"
-            Close-ExcelPackage -ExcelPackage $ExcelPackage -Show:$Show
-    }    
+        }
+        elseif ($ExcelWorkbook.Worksheets[$MoveAfter]) {
+            if ($MoveAfter -is [int]) {
+                $ExcelWorkbook.Worksheets.MoveAfter($ws.Index, $MoveAfter)
+            }
+            else {
+                $ExcelWorkbook.Worksheets.MoveAfter($WorksheetName, $MoveAfter)
+            }
+        }
+        else {Write-Warning "Can't find worksheet '$MoveAfter'; worsheet '$WorksheetName' will not be moved."}
+    }
+    #endregion
+    if ($Activate) {Select-Worksheet -ExcelWorksheet $ws  }
+    if ($ExcelPackage -and -not (Get-Member -InputObject $ExcelPackage -Name $ws.Name)) {
+        $sb = [scriptblock]::Create(('$this.workbook.Worksheets["{0}"]' -f $ws.name))
+        Add-Member -InputObject $ExcelPackage -MemberType ScriptProperty -Name $ws.name -Value $sb
+    }
+    return $ws
+}
