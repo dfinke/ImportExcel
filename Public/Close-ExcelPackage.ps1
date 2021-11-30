aa1b0427671c97ae4c135efd9363ad6b0e7c7e98
dfinke <finked@hotmail.com>
2021-11-30 15:50:11 -0500

    Add -ReZip to Close-ExcelPackage like in Export-Excel #1111

--- Public/Close-ExcelPackage.ps1
+++ Public/Close-ExcelPackage.ps1
@@ -1,33 +1,38 @@
 function Close-ExcelPackage {
     [CmdLetBinding()]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword","")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "")]
     param (
-        [parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [OfficeOpenXml.ExcelPackage]$ExcelPackage,
-        [switch]$Show,
+        [Switch]$Show,
         [Switch]$NoSave,
         $SaveAs,
         [ValidateNotNullOrEmpty()]
         [String]$Password,
-        [switch]$Calculate
+        [Switch]$Calculate,
+        [Switch]$ReZip
     )
-    if ( $NoSave)      {$ExcelPackage.Dispose()}
+
+    if ( $NoSave) { $ExcelPackage.Dispose() }
     else {
         if ($Calculate) {
-            try   { [OfficeOpenXml.CalculationExtension]::Calculate($ExcelPackage.Workbook) }
-            catch { Write-Warning "One or more errors occured while calculating, save will continue, but there may be errors in the workbook."}
+            try { [OfficeOpenXml.CalculationExtension]::Calculate($ExcelPackage.Workbook) }
+            catch { Write-Warning "One or more errors occured while calculating, save will continue, but there may be errors in the workbook." }
         }
         if ($SaveAs) {
             $SaveAs = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($SaveAs)
-            if ($Password) {$ExcelPackage.SaveAs( $SaveAs, $Password ) }
-            else           {$ExcelPackage.SaveAs( $SaveAs)}
+            if ($Password) { $ExcelPackage.SaveAs( $SaveAs, $Password ) }
+            else { $ExcelPackage.SaveAs( $SaveAs) }
         }
-        else         {
-            if ($Password) {$ExcelPackage.Save($Password) }
-            else           {$ExcelPackage.Save()          }
+        else {
+            if ($Password) { $ExcelPackage.Save($Password) }
+            else { $ExcelPackage.Save() }
             $SaveAs = $ExcelPackage.File.FullName
         }
+        if ($ReZip) {
+            Invoke-ExcelReZipFile -ExcelPackage $ExcelPackage
+        }
         $ExcelPackage.Dispose()
-        if ($Show)   {Start-Process -FilePath $SaveAs }
+        if ($Show) { Start-Process -FilePath $SaveAs }
     }
 }
