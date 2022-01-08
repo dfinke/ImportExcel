867495886533d31e1f32762a82eab8b23708f942
IT is Everywhere <itiseverywhere@users.noreply.github.com>
2022-01-08 09:05:20 +0100

    DynamicallyDetermineRangeBasedOnArrayColumnNumber
    This allows you to dynamically determine the Range based on the column number of the Array you choose to save as Excel file, which allows you to change and rearrange columns and keep the ConditionalTexts intact, as they move along to the correct column

--- Public/New-ConditionalText.ps1
+++ Public/New-ConditionalText.ps1
@@ -5,6 +5,7 @@ function New-ConditionalText {
         #[Parameter(Mandatory=$true)]
         [Alias('ConditionValue')]
         $Text,
+        $Comment,
         [Alias('ForeGroundColor')]
         $ConditionalTextColor=[System.Drawing.Color]::DarkRed,
         $BackgroundColor=[System.Drawing.Color]::LightPink,
@@ -24,19 +25,45 @@ function New-ConditionalText {
             'Expression'
         )]
         [Alias('RuleType')]
-        $ConditionalType='ContainsText'
+        $ConditionalType='ContainsText',
+        $Array,
+        $Column,
+        [Switch]$NoWarning
     )
+    $CommandName = $MyInvocation.MyCommand
 
-    $obj = [PSCustomObject]@{
-        Text                 = $Text
-        ConditionalTextColor = $ConditionalTextColor
-        ConditionalType      = $ConditionalType
-        PatternType          = $PatternType
-        Range                = $Range
-        BackgroundColor      = $BackgroundColor
-    }
+    If (-not$Array -and -not$NoWarning) {Write-Warning "$CommandName, Array is empty, column ""$Column"" and Text ""$Text"", Comment: ""$Comment"""}
+    If ($Column -eq "" -and -not$NoWarning) {Write-Warning "$CommandName, Column is empty, Text ""$Text"", Comment: ""$Comment"""}
 
-    $obj.pstypenames.Clear()
-    $obj.pstypenames.Add("ConditionalText")
-    $obj
+    #region determine Range based on Column
+    If ($Array -and $Column -ne "") {
+        $Columns = $Array[0].psobject.Properties | foreach { $_.Name }
+        $Range = ""
+        Foreach ($Col in $Columns) {
+            #Write-Verbose "Worksheet:$Worksheet`tColumn:$Column`tRange: $Range"
+            If ("$Col" -eq "$Column") {
+                $Iteration = [array]::IndexOf($Columns, $Column)
+                $ColName = Convert-NumberToA1 ($Iteration + 1)
+                $Range = "$ColName"+":"+"$ColName"
+                #Write-Verbose "Worksheet:$Worksheet`tColumn:$Column`tRange: $Range"
+            } # This is the Column we are looking for
+        }
+
+        If (-not$Range -and -not$NoWarning) {Write-Warning "$CommandName, Column ""$Column"" not found, Text ""$Text"", Comment: ""$Comment"""}
+        If ($Range) {
+
+            $obj = [PSCustomObject]@{
+                Text                 = $Text
+                ConditionalTextColor = $ConditionalTextColor
+                ConditionalType      = $ConditionalType
+                PatternType          = $PatternType
+                Range                = $Range
+                BackgroundColor      = $BackgroundColor
+            }
+            $obj.pstypenames.Clear()
+            $obj.pstypenames.Add("ConditionalText")
+            $obj
+        } # If ($Range)
+    } # If ($Array -and $Column -ne "")
+    #endregion determine Range based on Column  
 }
