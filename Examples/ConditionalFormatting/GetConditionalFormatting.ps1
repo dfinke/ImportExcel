c4eed8c25d5e16b87d9eaaca6dd347d02c52b140
Kinga Kazala <kkazala@ethz.ch>
2022-07-05 16:05:40 +0200

    GetConditionalFormatting.ps1 example added: exports conditional formatting rules in a 'ready to use' format

---
+++ Examples/ConditionalFormatting/GetConditionalFormatting.ps1
@@ -0,0 +1,10 @@
+﻿try { Import-Module $PSScriptRoot\..\..\ImportExcel.psd1 } catch { throw ; return}
+
+$xlSourcefile = "$env:TEMP\ImportExcelExample.xlsx"
+$excel = Open-ExcelPackage -Path $xlSourcefile
+
+$excel.Workbook.Worksheets | ForEach-Object {
+    $_.ConditionalFormatting | ForEach-Object {
+        Write-Host "Add-ConditionalFormatting -Worksheet `$excel[""$worksheetName""]  -Range '$($_.Address)'  -ConditionValue '$($_.Formula)' -RuleType $($_.Type) "
+    }
+}
