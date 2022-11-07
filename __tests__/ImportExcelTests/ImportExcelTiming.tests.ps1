a1986de292295f17918f4995c136752f5e2bdacd
dfinke <finked@hotmail.com>
2022-11-07 12:36:59 -0500

    update filename and check count returned

--- __tests__/ImportExcelTests/ImportExcelTiming.tests.ps1
+++ __tests__/ImportExcelTests/ImportExcelTiming.tests.ps1
@@ -3,9 +3,10 @@ Import-Module $PSScriptRoot\..\..\ImportExcel.psd1 -Force
 Describe "Tests Import-Excel Timings" -Tag Timing {
     It "Should read the 20k xlsx in -le 2100 milliseconds" {
         $timer = Measure-Command {
-            $null = Import-Excel $PSScriptRoot\Rows20k.xlsx
+            $data = Import-Excel $PSScriptRoot\TimingRows20k.xlsx
         }
 
         $timer.TotalMilliseconds | Should -BeLessOrEqual 2100        
+        $data.Count | Should -Be 19999
     }
 }
