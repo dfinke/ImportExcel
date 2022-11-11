652a6111376adaf3f0d5a8655af0acc09659779b
Thomas Hofkens <thomas.hofkens@savaco.com>
2022-11-12 00:24:17 +0100

    Added new function 'Set-CellComment'

---
+++ Public/Set-CellComment.ps1
@@ -0,0 +1,31 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope='Function', Target='Set*', Justification='Does not change system state')]
+param()
+
+function Set-CellComment {
+    [CmdletBinding(DefaultParameterSetName = "ColumnLetter")]
+    param(
+        [Parameter(Mandatory = $True)]
+        [OfficeOpenXml.ExcelWorkSheet]$Worksheet,
+
+        [Parameter(Mandatory = $True)]
+        [Int]$Row,
+
+        [Parameter(Mandatory = $True, ParameterSetName = "ColumnLetter")]
+        [String]$ColumnLetter,
+
+        [Parameter(Mandatory = $True, ParameterSetName = "ColumnNumber")]
+        [Int]$ColumnNumber,
+
+        [Parameter(Mandatory = $True)]
+        [String]$Text
+    )
+
+    If ($PSCmdlet.ParameterSetName -eq "ColumnNumber") {
+        $ColumnLetter = (Get-ExcelColumnName -ColumnNumber $ColumnNumber).ColumnName
+    }
+
+    $a = "{0}{1}" -f $ColumnLetter,$Row
+    $t = $Worksheet.Cells[$a]
+    [Void]$t.AddComment($Text, "ImportExcel")
+    $t.Comment.AutoFit = $True
+}
