83d9e27de8fc0ae0cb904cb37213f18984b00c89
Minio <oliwexminio@gmail.com>
2022-12-21 19:49:10 +0100

    Public
    Expand Get-ExcelSheetDimension to get more descriptive information about excel sheet

--- Public/Get-ExcelSheetDimensionAddress.ps1
+++ Public/Get-ExcelSheetDimensionAddress.ps1
@@ -1,15 +1,34 @@
-function Get-ExcelSheetDimensionAddress {
+function Get-ExcelSheetDimension {
     <#
         .SYNOPSIS
         Get the Excel address of the dimension of a sheet
 
+        .PARAMETER
+        Object of [OfficeOpenXml.ExcelWorksheet]
+
         .EXAMPLE
-        Get-ExcelSheetDimensionAddress $excel.Sheet1
+        $excelPackage=Open-ExcelPackage -Path "H:\Test\test.xlsx"
+        Get-ExcelSheetDimensionAddress -Worksheet $excelPackage.Workbook.Worksheets[1]
+        Close-ExcelPackage -ExcelPackage $excelPackage
+
+        .OUTPUT
+        RANGE        : I9:AJ78
+        END_Column   : 36
+        END_Row      : 78
+        START_Column : 9
+        START_Row    : 9
     #>
+    
     param(
         [Parameter(Mandatory)]
-        [OfficeOpenXml.ExcelWorksheet]$Worksheet 
+        [OfficeOpenXml.ExcelWorksheet]$Worksheet
     )
-
-    $Worksheet.Dimension.Address
+    $dimensionProperties=$Worksheet.Dimension
+    [PSCustomObject]@{
+        RANGE = $dimensionProperties.Address
+        END_Column = $dimensionProperties.End.Column
+        END_Row = $dimensionProperties.End.Row
+        START_Column = $dimensionProperties.Start.Column
+        START_Row = $dimensionProperties.Start.Column
+    }
 }
